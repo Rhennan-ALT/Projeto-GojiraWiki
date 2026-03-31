d21dec0cded5ddf71ed96dbeaf8f770bc515ea59
Rhennan <reehsecundario@gmail.com>
2026-03-31 18:09:25 -0300

    feat: Implementações diversas.

--- app.js
+++ app.js
@@ -1,3 +1,55 @@
 import { chars } from "./data/characters.js";
 
-console.log(chars)
+const app = document.getElementById("app");
+
+function render(chars) {
+  app.innerHTML = `
+    <h1><strong>${chars.nome}</strong></h1>
+    <br>
+    <h2> <strong>Lore</strong></h2>
+    <p>${chars.lore}</p>
+    <br>
+    <h2> <strong>Poderes</strong></h2>
+    <ul>
+        ${chars.powers}
+    </ul>
+    <br>
+    <h2><strong>Aparições</strong></h2>
+    <ul>
+        ${chars.apparitions}
+    </ul>
+    <br>
+    <img src="${chars.image}" class="w-full rounded-lg shadow-md">
+  `;
+}
+
+render(chars[0]);
+
+
+// Implementação do Carrossel de Imagens da Tela Inicial...
+
+const imgs = ['assets/imgs/cr1.jpg', 'assets/imgs/cr2.jpg', 'assets/imgs/cr3.jpeg',]
+
+let index = 0
+let interval = setInterval(next, 3000);
+
+function showImage() {
+    document.getElementById("carrossel").src = imgs[index];
+  }
+
+function resetAutoPlay() {
+  clearInterval(interval);
+  interval = setInterval(next, 4500);
+}
+
+function next() {
+  index = (index + 1) % imgs.length;
+  showImage();
+  resetAutoPlay();
+}
+
+function prev() {
+  index = (index - 1 + imgs.length) % imgs.length;
+  showImage();
+  resetAutoPlay();
+}
