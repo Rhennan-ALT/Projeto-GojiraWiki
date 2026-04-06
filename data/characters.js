7c8cdd59d6a64025c120c7b7143cff6de6ae14cc
Rhennan <reehsecundario@gmail.com>
2026-04-05 23:21:34 -0300

    Feat: Atualizaçao no json dos chars

--- data/characters.js
+++ data/characters.js
@@ -4,7 +4,7 @@ export const chars = [
 
         nome: 'Shin Godzilla (シン・ゴジラ Shin Gojira)',
         
-        lore: 'Em 2016, a Guarda Costeira do Japão investiga um iate abandonado na Baía de Tóquio . A embarcação é rapidamente destruída e a linha de transporte aquático da Baía de Tóquio fica inundada de sangue. Após assistir a um vídeo viral do incidente, o Subsecretário Chefe do Gabinete, Rando Yaguchi, acredita que a causa foi uma criatura viva, o que é confirmado por reportagens que mostram sua cauda emergindo do oceano. Pouco depois, a criatura se move para o interior, rastejando pelo distrito de Kamata e pela área de Shinagawa , em Tóquio , deixando um rastro de morte e destruição durante uma evacuação desorganizada e caótica. A criatura rapidamente evolui para uma forma bípede , mas sofre um superaquecimento e retorna ao mar. (continua)',
+        lore: 'Em 2016, a Guarda Costeira do Japão investiga um iate abandonado na Baía de Tóquio . A embarcação é rapidamente destruída e a linha de transporte aquático da Baía de Tóquio fica inundada de sangue. Após assistir a um vídeo viral do incidente, o Subsecretário Chefe do Gabinete, Rando Yaguchi, acredita que a causa foi uma criatura viva, o que é confirmado por reportagens que mostram sua cauda emergindo do oceano. Pouco depois, a criatura se move para o interior, rastejando pelo distrito de Kamata e pela área de Shinagawa , em Tóquio , deixando um rastro de morte e destruição durante uma evacuação desorganizada e caótica. A criatura rapidamente evolui para uma forma bípede , mas sofre um superaquecimento e retorna ao mar. (continua...)',
         
         powers: ['Sopro atômico roxo (que se subdivide em raios dorsais e da cauda);','alta resistência a ataques convencionais;', 'capacidade de adaptação imediata e a geração de formas de vida humanoides. '],
         
@@ -17,7 +17,7 @@ export const chars = [
         
         nome: "King Kong (キングコング, Kingu Kongu)",
         
-        lore: "Traduzido do inglês-King Kong, ou simplesmente Kong, é um monstro gigante, baseado no personagem homônimo criado por Edgar Wallace e Merian C. Cooper e pertencente à Universal Pictures. Ele é um dos principais protagonistas da franquia Monsterverse da Legendary Pictures. (continua)",
+        lore: "Traduzido do inglês-King Kong, ou simplesmente Kong, é um monstro gigante, baseado no personagem homônimo criado por Edgar Wallace e Merian C. Cooper e pertencente à Universal Pictures. Ele é um dos principais protagonistas da franquia Monsterverse da Legendary Pictures. (continua...)",
         
         powers: ["Força Sobrenatural, Inteligência e Adaptação;", "Agilidade e Velocidade;", "Resistência e Durabilidade, Garras e Dentes Afiados;", "Habilidades de Escalada, Habilidades de Natação, Rugido Poderoso;"],
         
